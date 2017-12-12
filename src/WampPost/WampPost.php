78c740c44bce22bba7a2b8a7c9b6f7eb3bb4e0cb
David Dan <davidwdan@gmail.com>
2017-12-12 09:31:13 -0500

    A bit of refactoring

--- src/WampPost/WampPost.php
+++ src/WampPost/WampPost.php
@@ -6,6 +6,7 @@ use Psr\Http\Message\ServerRequestInterface;
 use React\EventLoop\Factory;
 use React\EventLoop\LoopInterface;
 use React\Http\Response;
+use React\Http\Server as HttpServer;
 use React\Promise\Deferred;
 use React\Socket\Server;
 use Thruway\CallResult;
@@ -13,37 +14,24 @@ use Thruway\Common\Utils;
 use Thruway\Message\ErrorMessage;
 use Thruway\Peer\Client;
 
-class WampPost extends Client {
-    private $bindAddress;
-    private $realmName;
-
-    /** @var Server */
+class WampPost extends Client
+{
     private $socket;
-    private $http;
 
     public function __construct($realmName, LoopInterface $loop = null, $bindAddress = 'tcp://127.0.0.1:8181')
     {
-        if ($loop === null) {
-            $loop = Factory::create();
-        }
+        $loop = $loop ?: Factory::create();
+        $http = new HttpServer([$this, 'handleRequest']);
 
-        $this->bindAddress = $bindAddress;
-        $this->realmName = $realmName;
-        $this->socket = new Server($this->bindAddress, $loop);
-        $this->http = new \React\Http\Server([$this, 'handleRequest']);
+        $this->socket = new Server($bindAddress, $loop);
 
-        $this->http->listen($this->socket);
+        $http->listen($this->socket);
 
         parent::__construct($realmName, $loop);
     }
 
-    public function start($startLoop = true) {
-
-        parent::start($startLoop);
-    }
-
-    public function onSessionStart($session, $transport) {
-
+    public function onSessionStart($session, $transport)
+    {
     }
 
     /**
@@ -56,23 +44,27 @@ class WampPost extends Client {
         parent::onClose($reason);
     }
 
-    public function handleRequest(ServerRequestInterface $request, callable $next) {
-        if ($request->getUri()->getPath() == '/pub' && $request->getMethod() == 'POST') {
+    public function handleRequest(ServerRequestInterface $request, callable $next)
+    {
+        if ($request->getMethod() === 'POST' && $request->getUri()->getPath() === '/pub') {
             return $this->handlePublishHttpPost($request, $next);
-        } else if ($request->getUri()->getPath() == '/call' && $request->getMethod() == 'POST') {
-            return $this->handleCallHttpRequest($request, $next);
-        } else {
-            return new Response(404, [], 'Not found');
         }
+
+        if ($request->getMethod() === 'POST' && $request->getUri()->getPath() === '/call') {
+            return $this->handleCallHttpRequest($request, $next);
+        }
+
+        return new Response(404, [], 'Not found');
     }
 
-    private function handlePublishHttpPost(ServerRequestInterface $request, callable $next) {
+    private function handlePublishHttpPost(ServerRequestInterface $request, callable $next)
+    {
         try {
             //{"topic": "com.myapp.topic1", "args": ["Hello, world"]}
             $json = json_decode($request->getBody());
 
             if ($json === null) {
-                throw new \Exception("JSON decoding failed: " . json_last_error_msg());
+                throw new \Exception('JSON decoding failed: ' . json_last_error_msg());
             }
 
             if (isset($json->topic)
@@ -83,23 +75,24 @@ class WampPost extends Client {
             ) {
                 $json->topic = strtolower($json->topic);
                 if (!Utils::uriIsValid($json->topic)) {
-                    throw new \Exception("Invalid URI: " . $json->topic);
+                    throw new \Exception('Invalid URI: ' . $json->topic);
                 }
 
-                $argsKw = isset($json->argsKw) && is_object($json->argsKw) ? $json->argsKw : null;
+                $argsKw  = isset($json->argsKw) && is_object($json->argsKw) ? $json->argsKw : null;
                 $options = isset($json->options) && is_object($json->options) ? $json->options : null;
                 $this->getSession()->publish($json->topic, $json->args, $argsKw, $options);
             } else {
-                throw new \Exception("Invalid request: " . json_encode($json));
+                throw new \Exception('Invalid request: ' . json_encode($json));
             }
         } catch (\Exception $e) {
-            return new Response(400, [], "Bad Request: " . $e->getMessage());
+            return new Response(400, [], 'Bad Request: ' . $e->getMessage());
         }
 
         return new Response(200, [], 'pub');
     }
 
-    private function handleCallHttpRequest(ServerRequestInterface $request, callable $next) {
+    private function handleCallHttpRequest(ServerRequestInterface $request, callable $next)
+    {
         $deferred = new Deferred();
         try {
             //{"procedure": "com.myapp.procedure1", "args": ["Hello, world"], "argsKw": {}, "options": {} }
@@ -109,15 +102,15 @@ class WampPost extends Client {
                 && Utils::uriIsValid($json->procedure)
                 && ($this->getCaller() !== null)
             ) {
-                $args = isset($json->args) && is_array($json->args) ? $json->args : null;
-                $argsKw = isset($json->argsKw) && is_object($json->argsKw) ? $json->argsKw : null;
+                $args    = isset($json->args) && is_array($json->args) ? $json->args : null;
+                $argsKw  = isset($json->argsKw) && is_object($json->argsKw) ? $json->argsKw : null;
                 $options = isset($json->options) && is_object($json->options) ? $json->options : null;
 
                 $this->getSession()->call($json->procedure, $args, $argsKw, $options)->then(
-                    /** @param CallResult $result */
+                /** @param CallResult $result */
                     function (CallResult $result) use ($deferred) {
                         $responseObj          = new \stdClass();
-                        $responseObj->result  = "SUCCESS";
+                        $responseObj->result  = 'SUCCESS';
                         $responseObj->args    = $result->getArguments();
                         $responseObj->argsKw  = $result->getArgumentsKw();
                         $responseObj->details = $result->getDetails();
@@ -126,7 +119,7 @@ class WampPost extends Client {
                     },
                     function (ErrorMessage $msg) use ($deferred) {
                         $responseObj                = new \stdClass();
-                        $responseObj->result        = "ERROR";
+                        $responseObj->result        = 'ERROR';
                         $responseObj->error_uri     = $msg->getErrorURI();
                         $responseObj->error_args    = $msg->getArguments();
                         $responseObj->error_argskw  = $msg->getArgumentsKw();
@@ -138,11 +131,11 @@ class WampPost extends Client {
                 );
             } else {
                 // maybe return an error code here
-                $deferred->resolve(new Response(200, [], "No procedure set"));
+                $deferred->resolve(new Response(200, [], 'No procedure set'));
             }
         } catch (\Exception $e) {
             // maybe return an error code here
-            $deferred->resolve(new Response(200, [], "Problem"));
+            $deferred->resolve(new Response(200, [], 'Problem'));
         }
 
         return $deferred->promise();
